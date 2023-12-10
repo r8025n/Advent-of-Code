79c9ff3d514c31910a16cbe4a2d300591da53ae2
r8025n <raihanshovon95@gmail.com>
2023-12-10 22:39:52 +0600

    Add solution to day 5 part 2
    Optimized 0.016s

--- 2023/5_2.py
+++ 2023/5_2.py
@@ -1,50 +1,25 @@
+import copy
+
 input_file = open('input.txt', 'r')
 input_lines = input_file.readlines()
 
 map_of_maps = {1: {}, 2: {}, 3:{}, 4:{}, 5:{}, 6:{}, 7:{}}
-map_number = 0
-location_list = []
+result = 999999999999
 
-def recursive_approach(level, seed_list):
-    for val in seed_list:
-        list_for_next_level = []
-        in_range = False
-        for rrange, difference in map_of_maps[level].items():
-            if val >= rrange[1] and val <= rrange[2]:
-                list_for_next_level.append(val + difference)
-                in_range = True
-                
-        if in_range == False:
-            list_for_next_level.append(val)
-            in_range = False
-        if (level + 1) <= 7:
-            recursive_approach(level + 1, list_for_next_level)
-        else:
-            location_list.append(min(list_for_next_level))
+def get_seed_list():
+    seed_range_list = []
+    seed_line = input_lines[0]
+    seed_line = seed_line.split(":")[1].strip()
+    seed_pairs = [int(x) for x in seed_line.split()]
 
-    return
-
-def get_seed_list(seed_pairs):
-    seeds = []
     for i in range(0, len(seed_pairs), 2):
         start = seed_pairs[i]
         rrange = seed_pairs[i + 1]
-        for i in range(0, rrange + 1):
-            seeds.append(start + i)
-    # print("/////////////////////")        
-    # print(len(seeds))
-    # print(len(set(seeds)))
-    print("/////////////////////")
-    return seeds
+        seed_range_list.append([start, start + rrange - 1])
+    return seed_range_list
 
-
-if __name__=="__main__":
-    seed_line = input_lines[0]
-    seed_line = seed_line.split(":")[1].strip()
-
-    seed_pairs = [int(x) for x in seed_line.split()]
-
-    seeds = get_seed_list(seed_pairs)
+def generate_map_of_maps():
+    map_number = 0
 
     for i in range(1, len(input_lines)):
         line = input_lines[i]
@@ -55,10 +30,37 @@ if __name__=="__main__":
             continue
         else:
             destination, source, number_range = [int(x) for x in line.split()]
-            source_range = (i, source, source + number_range) #i is added to make the key unique
+            source_range = (source, source + number_range)
             difference_between_destination_and_source = destination - source
             map_of_maps[map_number][source_range] = difference_between_destination_and_source
+    return
 
-    recursive_approach(1, seeds)
+if __name__=="__main__":
+    ranges_for_next_level = []
+    seed_range_list = get_seed_list()
+    generate_map_of_maps()
 
-    print(min(location_list))
+    for i in range(1, 8):
+        ranges_for_next_level = []
+        current_map = map_of_maps[i]
+        for seed_range in seed_range_list:
+            found = False
+            for rrange, difference in current_map.items():
+                if seed_range[1] > rrange[0] and seed_range[1] < rrange[1]:
+                    found = True
+                    new_range = [max(seed_range[0], rrange[0]) + difference, seed_range[1] + difference]
+                    ranges_for_next_level.append(new_range)
+                    if seed_range[0] < rrange[0]:
+                        extra_range =  [seed_range[0], rrange[0] - 1]
+                        seed_range_list.append(extra_range)
+                    break
+            if found == False:
+                ranges_for_next_level.append(seed_range)
+        seed_range_list = copy.deepcopy(ranges_for_next_level)
+
+    
+    for seed_range in seed_range_list:
+        result = min(result, seed_range[0])
+
+    print(result)
+
