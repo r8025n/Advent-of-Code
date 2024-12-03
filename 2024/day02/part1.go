aec4194d6ebba67b875dc43000f60b23b587d97c
r8025n <raihanshovon95@gmail.com>
2024-12-03 21:20:44 +0600

    Update part1 algorithm

--- 2024/day02/part1.go
+++ 2024/day02/part1.go
@@ -1,32 +1,25 @@
 package main
 
 import(
-	"fmt"
+	// "fmt"
+	"math"
 )
 
 func solvePart1(reports [][]int) int{
 	safe := 0
-	// for report, _ := range(reports){
-	// 	fmt.Println(report)
-	// }
 	outer:
 	for _, report := range(reports) {
-		fmt.Println(report)
-		if report[0] < report[len(report) - 1] {
-			for i := 1; i < len(report); i++ {
-				diff := report[i] - report[i - 1]
-				if report[i] < report[i - 1] || diff > 3 || diff < 1 {
-					continue outer
-				}
+		totalDiff := int(math.Abs(float64(report[0]) - float64(report[len(report) - 1])))
+		sumOfDiff := 0
+
+		for i := 1; i < len(report); i++ {
+			lvlDiff := int(math.Abs(float64(report[i]) - float64(report[i - 1])))
+			if lvlDiff < 1 || lvlDiff > 3 {
+				continue outer
 			}
-			safe += 1
-		} else {
-			for i := 1; i < len(report); i++ {
-				diff := report[i - 1] - report[i]
-				if report[i] > report[i - 1] || diff > 3 || diff < 1 {
-					continue outer
-				}
-			}
+			sumOfDiff += lvlDiff
+		}
+		if totalDiff == sumOfDiff {
 			safe += 1
 		}
 	}
