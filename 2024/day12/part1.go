c61195b6cc0542a179b0fb51d9e9687379d23864
r8025n <raihanshovon95@gmail.com>
2024-12-23 01:01:35 +0600

    Refactor

--- 2024/day12/part1.go
+++ 2024/day12/part1.go
@@ -4,15 +4,12 @@ import(
 	// "fmt"
 )
 
-var XX = []int{0, 0, 1, -1}
-var YY = []int{1, -1, 0, 0}
-
-func bfsSolve(input [][]rune, visited [][]int, i, j int) int {
+func bfsSolvePart1(input [][]rune, visited [][]int, i, j int) int {
 	q := Queue{}
 	target := input[i][j]
-	area, perimeter := 1, 0
-	input[i][j] = 0
+	areas, perimeters := 1, 0
 	visited[i][j] = 1
+	input[i][j] = 0
 	q.Enqueue([]int{i, j})
 
 	for !q.Empty() {
@@ -24,7 +21,7 @@ func bfsSolve(input [][]rune, visited [][]int, i, j int) int {
 			yy := y + YY[k]
 
 			if xx < 0 || yy < 0 || xx >= len(input) || yy >= len(input[0]) {
-				perimeter++
+				perimeters++
 				continue
 			}
 
@@ -35,30 +32,28 @@ func bfsSolve(input [][]rune, visited [][]int, i, j int) int {
 			if input[xx][yy] == target {
 				q.Enqueue([]int{xx, yy})
 				visited[xx][yy] = 1
+				areas++
 				input[xx][yy] = 0
-				area++
 			} else {
-				perimeter++
+				perimeters++
 			}
 		}
 	}
 
-	return area * perimeter
+	return areas * perimeters
 }
 
 func solvePart1(input [][]rune) int {
 	output := 0
 
 	for i := 0; i < len(input); i++ {
-		for j := 0; j < len(input); j++ {
+		for j := 0; j < len(input[0]); j++ {
+			visited := make([][]int, len(input))
+			for i := 0; i < len(visited); i++ {
+				visited[i] = make([]int, len(input[0]))
+			}
 			if input[i][j] != 0 {
-				visited := make([][]int, len(input))
-
-				for i := 0; i < len(visited); i++ {
-					visited[i] = make([]int, len(input[0]))
-				}
-
-				output += bfsSolve(input, visited, i, j)
+				output += bfsSolvePart1(input, visited, i, j)
 			}
 		}
 	}
---
+++ 2024/day12/utils.go
@@ -0,0 +1,4 @@
+package main
+
+var XX = []int{0, 0, 1, -1}
+var YY = []int{1, -1, 0, 0}
