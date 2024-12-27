16ec2a5e1b57dea12b2d898a2371afa0730cc30a
r8025n <raihanshovon95@gmail.com>
2024-12-27 21:52:26 +0600

    Remove unnecessary comments

--- 2024/day16/part1.go
+++ 2024/day16/part1.go
@@ -23,7 +23,6 @@ var directionChangeToCostMap = map[string]int{
 
 func dfsSolve(input []string, visited [][]int, x, y, cost int, dirChange string) {
 	if _, exists := directionChangeToCostMap[dirChange]; !exists {
-		// fmt.Println(dirChange)
 		return
 	}
 
@@ -48,18 +47,12 @@ func dfsSolve(input []string, visited [][]int, x, y, cost int, dirChange string)
 		return
 	}
 
-	// opt1 := directionChangeToCostMap[dirChange] + dfsSolve(input, visited, x + 1, y, string(dirChange[1]) + "v")
-	// opt2 := directionChangeToCostMap[dirChange] + dfsSolve(input, visited, x - 1, y, string(dirChange[1]) + "^")
-	// opt3 := directionChangeToCostMap[dirChange] + dfsSolve(input, visited, x, y + 1, string(dirChange[1]) + ">")
-	// opt4 := directionChangeToCostMap[dirChange] + dfsSolve(input, visited, x, y - 1, string(dirChange[1]) + "<")
-	//
 	dfsSolve(input, visited, x + 1, y, newCost, string(dirChange[1]) + "v")
 	dfsSolve(input, visited, x - 1, y, newCost, string(dirChange[1]) + "^")
 	dfsSolve(input, visited, x, y + 1, newCost, string(dirChange[1]) + ">")
 	dfsSolve(input, visited, x, y - 1, newCost, string(dirChange[1]) + "<")
 
 	return
-	// return min([]int{opt1, opt2, opt3, opt4})
 }
 
 func solvePart1(input []string) int {
@@ -72,16 +65,9 @@ func solvePart1(input []string) int {
 		}
 	}
 
-	// for _, line := range input {
-	// 	fmt.Println(line)
-	// }
 	startX, startY := findTarget(input, 'S')
 	finishX, finishY := findTarget(input, 'E')
 	dfsSolve(input, visited, startX, startY, 0, "_>")
-
-	// for _, row := range visited {
-	// 	fmt.Println(row)
-	// }
 
 	return visited[finishX][finishY]
 }
